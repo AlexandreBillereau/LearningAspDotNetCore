6cbdcd8a8376a53c95eeb528ee1f84d5b3c2c0b5
alxeandre <alexandre.mr.billerau@gmail.com>
2023-05-16 18:09:16 -0400

    add Model + DefaultCoonection

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models;
+
+public class Category
+{
+  [Key]
+  public int ID { get; set; }
+  public string Name { get; set; }
+  public int DisaplayOrder { get; set; }
+  public DateTime createDateTime { get; set; } = DateTime.Now;
+  
+
+ }
+
