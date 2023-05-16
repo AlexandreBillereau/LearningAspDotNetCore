92c27604c6a3b561a338d214a0f0b29c723ce6b8
alxeandre <alexandre.mr.billerau@gmail.com>
2023-05-16 19:04:39 -0400

    manipulation de la base de données

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace BulkyBook.Controllers;
 public class CategoryController : Controller
 {
+  private readonly ApplicationDBContext _db;
+
+  public CategoryController(ApplicationDBContext db)
+  {
+    _db = db;
+  }
+
   public IActionResult Index()
   {
+    var objCategoryList = _db.categories.ToList();
     return View();
   }
 }
